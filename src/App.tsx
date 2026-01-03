14be6a0a0e8d384e1015d4f0c2218b553018d3c7
dimitriserotokritou <derotokritou97@gmail.com>
2026-01-03 18:24:24 +0200

    changes to about window

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,38 @@ import { AboutWidnow } from "./components/AboutWindow/AboutWindow";
 import { ThemeToggle } from "./components/ThemeToggle/ThemeToggle";
 
 function App() {
-	return (
-		<>
-			<ThemeToggle />
-			<div className="container-fluid">
-				<section id="hero-about">
-					<div className="row mt-5">
-						<div className="col-6">
-							<h1 className="text-start">Dimitris Erotokritou</h1>
-							<h2 className="text-start">Software Developer</h2>
-						</div>
-						<div className="col-6">
-							<AboutWidnow />
-							<h1 className="text-center">Something Inspiring</h1>
-						</div>
-					</div>
-				</section>
-				<div className="row mt-5">
-					<div className="col w-100">
-						<h1 className="text-center">Skills</h1>
-					</div>
-				</div>
-				<div className="row mt-5">
-					<div className="col w-100">
-						<h1 className="text-center">Projects</h1>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <ThemeToggle />
+      <div className="container-fluid">
+        <section id="hero-about">
+          <div className="row mt-5 mb-5">
+            <div className="col-6 d-flex flex-column justify-content-center ps-5">
+              <h1 className="text-start">Dimitris Erotokritou</h1>
+              <h2 className="text-start">Software Developer</h2>
+            </div>
+            <div className="col-6">
+              <AboutWidnow />
+            </div>
+          </div>
+        </section>
+        <div className="row mt-5">
+          <div className="col w-100">
+            <h1 id="stack" className="text-center">
+              Stack
+            </h1>
+          </div>
+        </div>
+        <div className="row mt-5">
+          <div className="col w-100">
+            <h1 id="projects" className="text-center">
+              Projects
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/AboutWindow/AboutWindow.tsx
+++ src/components/AboutWindow/AboutWindow.tsx
@@ -2,14 +2,50 @@ import { RiCheckboxBlankCircleFill } from "react-icons/ri";
 import "./_aboutWindow.scss";
 
 export const AboutWidnow: React.FC = ({}) => {
-	return (
-		<div className="w-100 h-100 rounded-4 border">
-			<div className="d-flex justify-content-around m-3 window-icons-width">
-				<RiCheckboxBlankCircleFill color="red" size={"15px"} />
-				<RiCheckboxBlankCircleFill color="yellow" size={"15px"} />
-				<RiCheckboxBlankCircleFill color="green" size={"15px"} />
-			</div>
-			<hr />
-		</div>
-	);
+  return (
+    <div className="w-100 h-100 rounded-4 border">
+      <div className="d-flex justify-content-around m-3 window-icons-width">
+        <RiCheckboxBlankCircleFill color="red" size={"15px"} />
+        <RiCheckboxBlankCircleFill color="yellow" size={"15px"} />
+        <RiCheckboxBlankCircleFill color="green" size={"15px"} />
+      </div>
+      <hr />
+      <section id="about-section">
+        <div className="text-start m-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quo,
+          quisquam aliquid nisi harum minima sapiente maiores modi, dignissimos
+          omnis nobis id amet pariatur, aliquam optio veritatis doloribus odio
+          corrupti.
+        </div>
+      </section>
+      <hr />
+      <div className="d-flex">
+        <div className="d-flex">
+          <button type="button" className="btn mt-3 mb-3 ms-3">
+            <a
+              href="#stack"
+              className="link-underline link-underline-opacity-0"
+            >
+              Stack
+            </a>
+          </button>
+          <button type="button" className="btn m-3">
+            <a
+              href="#projects"
+              className="link-underline link-underline-opacity-0"
+            >
+              Projects
+            </a>
+          </button>
+        </div>
+        <div className="d-flex justify-content-end w-100">
+          <button type="button" className="btn btn-outline-secondary m-3">
+            <a href="#" className="link-underline link-underline-opacity-0">
+              Download CV
+            </a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
