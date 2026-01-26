a5d65801f11edbabaf7b8abff24648264b739467
Dimitris <dimitris.erotokritou@sysdocgroup.com>
2026-01-26 17:48:47 +0200

    changed tech stack carousel component and scss

--- src/components/TechStack/TechStack.tsx
+++ src/components/TechStack/TechStack.tsx
@@ -1,22 +1,26 @@
+import React from "react";
 import "../../styles/_techStack.scss";
 import { TECH_STACK_ICONS } from "../../constants";
 
 export const TechStack: React.FC = () => {
-	const techStackIcons = TECH_STACK_ICONS;
-
 	return (
 		<div className="tech-stack-wrapper">
 			<div className="tech-stack-track">
-				{techStackIcons.map(({ key, icon: Icon }) => (
-					<div key={`first-${key}`} className="tech-stack-item">
-						<Icon size={45} />
-					</div>
-				))}
-				{techStackIcons.map(({ key, icon: Icon }) => (
-					<div key={`first-${key}`} className="tech-stack-item">
-						<Icon size={45} />
-					</div>
-				))}
+				<div className="tech-stack-set">
+					{TECH_STACK_ICONS.map(({ key, icon: Icon }) => (
+						<div key={`a-${key}`} className="tech-stack-item" title={key}>
+							<Icon className="tech-stack-icon" />
+						</div>
+					))}
+				</div>
+
+				<div className="tech-stack-set" aria-hidden="true">
+					{TECH_STACK_ICONS.map(({ key, icon: Icon }) => (
+						<div key={`b-${key}`} className="tech-stack-item" title={key}>
+							<Icon className="tech-stack-icon" />
+						</div>
+					))}
+				</div>
 			</div>
 		</div>
 	);
